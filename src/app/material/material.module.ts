3952571d65566b6d7e6d2b93b8a43621f1a6ab77
Rishabh Dubey <rishabhdubey020@gmail.com>
2020-05-16 15:33:45 +0530

    created sidenav

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
-const modules = [MatButtonModule, MatSidenavModule];
+const modules = [MatButtonModule, MatSidenavModule, MatIconModule];
 
 @NgModule({
   exports: [...modules],
