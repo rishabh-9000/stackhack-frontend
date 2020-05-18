5b6000998df79cd4ca8304a07e5f58ed0ef4c288
Rishabh Dubey <rishabhdubey020@gmail.com>
2020-05-18 18:14:20 +0530

    created routes and home and admin modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [{ path: 'register', component: UserComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
